287fa79f1c6c5ea5b2a91971243eb83a36cc9fbb
Adam Wiśniewski <norr.pl@gmail.com>
2022-06-29 15:30:40 +0200

    Add TTE

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import sys
 from pathlib import Path
 
-
 from PySide2 import QtCore, QtWidgets, QtGui
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+
+from Equations import Equation
 from Overrides.QPushButton import QPushButton
 
-WINDOW_WIDTH = 320
-WINDOW_HEIGHT = 300
+WINDOW_WIDTH = 330
+WINDOW_HEIGHT = 600
 WINDOW_TITLE = "Kalkulator dietetyczny"
-CRITICAL_BMI_COLOR = "#cc3300"
-BAD_BMI_COLOR = "#ff9966"
-WARING_BMI_COLOR = "#ffcc00"
-GOOD_BMI_COLOR = "#339900"
+
 TEXT_INPUT_FIELD_WIDTH = 50
 CALENDAR_INPUT_WIDTH = 85
 FEMALE_ICON = str(Path("icons/woman_female_avatar_icon_128.png").absolute())
@@ -24,7 +22,17 @@ GRID_ROW_MINIMUM_HEIGHT = 45
 RESULTS_FONT_FAMILY = 'Helvetica'
 RESULTS_FONT_SIZE = 16
 BMR_TOOLTIP_METHOD = "Liczony metodą Mifflin-St Jeor"
-
+tte_items = [
+                {"name": "1,2", "value": 1.2, "description": "osoba chora leżąca w łóżku"},
+                {"name": "1,25", "value": 1.25, "description": "pracownika biurowey, osoba o bardzo niskiej "
+                                                               "aktywności fizycznej związanej tylko z obowiązkami "
+                                                               "domowymi."},
+                {"name": "1,5", "value": 1.5, "description": "pracownik biurowy, który trenuje ok. 3 razy w tygodniu "
+                                                             "przez co najmniej godzinę."},
+                {"name": "1,75", "value": 1.75, "description": "osoba prowadząca aktywny tryb życia."},
+                {"name": "2", "value": 2, "description": "sportowiec trenujący co najmniej 6 godzin tygodniowo lub"
+                                                         " osoba, która wykonuje bardzo ciężką pracę fizyczną."},
+            ]
 
 
 class AppWidget(QtWidgets.QWidget):
@@ -51,6 +59,11 @@ class AppWidget(QtWidgets.QWidget):
         self.BMI_widget = QtWidgets.QLabel()
         self.BMR_label = QtWidgets.QLabel()
         self.BMR_widget = QtWidgets.QLabel()
+        self.BMI_label = QtWidgets.QLabel()
+        self.BMI_widget = QtWidgets.QLabel()
+        self.TEE_label = QtWidgets.QLabel("Aktywność fizyczna:")
+        self.TEE_widget = QtWidgets.QComboBox()
+        self.TTE_description_table = QtWidgets.QTableWidget(len(tte_items), 2, self)
         self.description_widget = QtWidgets.QLabel()
         self.calculate = QtWidgets.QPushButton("Oblicz")
 
@@ -87,6 +100,8 @@ class AppWidget(QtWidgets.QWidget):
         self.BMR_label.setToolTip(BMR_TOOLTIP_METHOD)
         self.BMR_widget.setFont(QtGui.QFont(RESULTS_FONT_FAMILY, RESULTS_FONT_SIZE))
         self.BMR_label.setFont(QtGui.QFont(RESULTS_FONT_FAMILY, RESULTS_FONT_SIZE))
+        self.add_tte_items(tte_items, self.TEE_widget)
+        self.generate_description_table(tte_items, self.TTE_description_table)
         self.weight_widget.setFixedWidth(TEXT_INPUT_FIELD_WIDTH)
         self.height_widget.setFixedWidth(TEXT_INPUT_FIELD_WIDTH)
         self.birth_date_widget.setFixedWidth(CALENDAR_INPUT_WIDTH)
@@ -101,88 +116,75 @@ class AppWidget(QtWidgets.QWidget):
         window_layout.addWidget(self.weight_widget, 2, 1)
         window_layout.addWidget(self.height_label, 3, 0)
         window_layout.addWidget(self.height_widget, 3, 1)
-        window_layout.addWidget(self.calculate, 4, 1)
-        window_layout.addWidget(self.BMI_label, 5, 0)
-        window_layout.addWidget(self.BMI_widget, 6, 0)
-        window_layout.addWidget(self.BMR_label, 5, 1)
-        window_layout.addWidget(self.BMR_widget, 6, 1)
-        window_layout.addWidget(self.description_widget, 7, 0, 1, 2)
-        self.calculate.clicked.connect(lambda: self.calculate_bmi())
+        window_layout.addWidget(self.TEE_label, 4, 0)
+        window_layout.addWidget(self.TEE_widget, 4, 1)
+        window_layout.addWidget(self.calculate, 5, 1)
+        window_layout.addWidget(self.TTE_description_table, 6, 0, 1, 3)
+        window_layout.addWidget(self.BMI_label, 7, 0)
+        window_layout.addWidget(self.BMI_widget, 8, 0)
+        window_layout.addWidget(self.BMR_label, 7, 1)
+        window_layout.addWidget(self.BMR_widget, 8, 1)
+        window_layout.addWidget(self.description_widget, 9, 0, 1, 2)
+        self.calculate.clicked.connect(lambda: self.calculate_results())
 
         return window_layout
 
-    def calculate_bmi(self):
+    def calculate_results(self):
         birthdate = self.birth_date_widget.date().toPython()
         age = (relativedelta(datetime.today().date(), birthdate)).years
-        height = self.height_widget.text().replace(",", ".")
-        weight = self.weight_widget.text().replace(",", ".")
-        try:
-            height = int(height) / 100
-        except ValueError:
-            return QtWidgets.QMessageBox.critical(self, "Wystąpił błąd", "Wzrost musi być liczbą")
+        calculation = Equation(weight=self.weight_widget.text(),
+                               height=self.height_widget.text(),
+                               sex=self.sex_button_group.checkedButton().get_button_name(),
+                               age=age,
+                               tte=self.TEE_widget.currentData())
 
-        try:
-            weight = float(weight)
-        except ValueError:
-            return QtWidgets.QMessageBox.critical(self, "Wystąpił błąd", "Waga musi być liczbą")
 
-        BMI = round((weight / height ** 2), 1)
-        # BMR equation using Mifflin-St Jeor method
-        BMR = int((9.99 * weight) + (6.25 * (height * 100)) - (4.92 * age))
-        if self.sex_button_group.checkedButton().get_button_name() == "f":
-            BMR -= 161
-        else:
-            BMR += 5
-
-        BMI_value = f'<font color="{self.get_description(bmi_result=BMI).get("color", "green")}">' \
-                       f'{str(BMI)}</font>'
+        BMI_value = f'<font color="{calculation.get_BMI_description().get("color", "green")}">' \
+                    f'{str(calculation.BMI())}</font>'
         self.BMI_label.setText("BMI")
         self.BMI_widget.setText(BMI_value)
 
         self.BMR_label.setText("BMR")
-        self.BMR_widget.setText(f'{str(BMR)} kcal')
-        self.description_widget.setText(self.get_description(bmi_result=BMI).get("description", "brak opisu"))
+        self.BMR_widget.setText(f'{str(calculation.BMR())} kcal')
+        self.description_widget.setText(calculation.get_BMI_description().get("description", "brak opisu"))
+        print(calculation.TTE())
 
 
-    def get_description(self, bmi_result: float) -> int | dict[str, str | None]:
-        """Method that's return description of BMI result and color.
+    def add_tte_items(self, items: list[dict], widget: QtWidgets.QComboBox):
+        if type(items) not in [list, tuple]:
+            raise ValueError(f"Items detected typ: {type(items)}. Items must be a `list` or `tuple`.")
+            return QtWidgets.QMessageBox.critical(self, "Wystąpił błąd", "Nieprawidłowy typ wartości listy.")  # noqa
+        for item in items:
+            widget.addItem(item["name"], item["value"])
 
-        :param bmi_result: `float` numer of calculated result.
-        :return: `dict` description and color message
-        """
+    def generate_description_table(self, rows: list[dict], table: QtWidgets.QTableWidget):
+        if type(rows) not in [list, tuple]:
+            raise ValueError(f"Items detected typ: {type(rows)}. Items must be a `list` or `tuple`.")
+            return QtWidgets.QMessageBox.critical(self, "Wystąpił błąd", "Nieprawidłowy typ wartości listy.")  # noqa
 
-        result = {
-            "color": None,
-            "description": None,
-        }
-        if 3 <= bmi_result < 16.0:
-            result["color"] = CRITICAL_BMI_COLOR
-            result["description"] = "wygłodzenie"
-        elif 16 <= bmi_result <= 17:
-            result["color"] = BAD_BMI_COLOR
-            result["description"] = "wychudzenie (spowodowane często przez ciężką chorobę)"
-        elif 17 <= bmi_result <= 18.5:
-            result["color"] = WARING_BMI_COLOR
-            result["description"] = "niedowaga"
-        elif 18.5 <= bmi_result < 25:
-            result["color"] = GOOD_BMI_COLOR
-            result["description"] = "waga prawidłowa"
-        elif 25 <= bmi_result < 30:
-            result["color"] = WARING_BMI_COLOR
-            result["description"] = "nadwaga"
-        elif 30 <= bmi_result < 35:
-            result["color"] = BAD_BMI_COLOR
-            result["description"] = "I stopień otyłości"
-        elif 35 <= bmi_result < 40:
-            result["color"] = CRITICAL_BMI_COLOR
-            result["description"] = "II stopień otyłości"
-        elif 40 <= bmi_result <= 720:
-            result["color"] = CRITICAL_BMI_COLOR
-            result["description"] = "III stopień otyłości (otyłość skrajna)"
-        else:
-            return QtWidgets.QMessageBox.critical(self, "Wystąpił błąd", "Podano nieprawidłową wagę lub wzrost")  # noqa
+        table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        table.setFocusPolicy(QtCore.Qt.NoFocus)
+        table.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
+        table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Fixed)
+        table.setHorizontalHeaderLabels(["Warość", "Opis"])
+        table.setColumnWidth(0, 56)
+        table.setColumnWidth(1, 250)
+        row_number = 0
+        table_height = 0
 
-        return result
+        for row in rows:
+            table.setItem(row_number, 0, QtWidgets.QTableWidgetItem(row["name"]))
+            table.setItem(row_number, 1, QtWidgets.QTableWidgetItem(row["description"]))
+            row_number += 1
+
+        table.resizeRowsToContents()
+
+        for row in range(table.rowCount()):
+            table_height += table.rowHeight(row) + 0.5
+
+        table_height += table.horizontalHeader().height()
+        table.setFixedHeight(table_height)
+        table.verticalHeader().hide()
 
 
 if __name__ == '__main__':
